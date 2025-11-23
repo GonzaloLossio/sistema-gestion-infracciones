080edd6a7cdb19759c4bd2a32affcca22dfedc83
Gonzalo Ernesto Lossio Brignardello <gonzalolozioqwer@gmail.com>
2025-11-23 13:52:21 -0500

    Add files via upload

---
+++ Bibliotecas/Fecha.h
@@ -0,0 +1,13 @@
+//
+// Created by Gonzalo on 21/11/2025.
+//
+
+#ifndef ESTRUCTURAS_LABORATORIO09_2025_1__FECHA_H
+#define ESTRUCTURAS_LABORATORIO09_2025_1__FECHA_H
+struct Fecha {
+    int dd;
+    int mm;
+    int aa;
+    int fecha;
+};
+#endif //ESTRUCTURAS_LABORATORIO09_2025_1__FECHA_H
